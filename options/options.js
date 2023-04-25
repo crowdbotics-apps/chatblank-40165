fde01e950d8260809c14d7983a13ad42e7346276
Guilherme Livramento <guilherme.livramento.fsl@crowdbotics.com>
2023-04-25 09:24:56 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "chatblank-40165",
   "url": "https://chatblank-40165.botics.co",
   "api": "https://chatblank-40165.botics.co/api/v1",
-  "initialRoute": "social-login"
+  "initialRoute": "chat"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
